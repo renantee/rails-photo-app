27da986cf176c175107eea67dc4470c29e59c3e6
Renante D. Entera <renante_entera@ligph.com>
2021-03-30 18:22:07 +0800

    Add Payment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
