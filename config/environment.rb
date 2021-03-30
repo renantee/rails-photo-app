1dd28c8c58c3bc2aacdf4ef0903b4154d9528ad2
Renante D. Entera <renante_entera@ligph.com>
2021-03-30 15:06:52 +0800

    Setup sending email using Mailtrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Sending email using Mailtrap
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5,
+  :user_name => Rails.application.credentials.mailtrap[:user_name],
+  :password => Rails.application.credentials.mailtrap[:password],
+  :domain => 'smtp.mailtrap.io'
+}
